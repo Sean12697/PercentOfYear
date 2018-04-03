78e5551dd58c43b55cbd6a80c4992221c86fb686
Sean O'Mahoney <seandavidomahoney@gmail.com>
2018-04-03 14:00:24 +0100

    FASTER

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.addEventListener('load', function() { showProgress(); setInterval(showProgress, 1000); });
+window.addEventListener('load', function() { showProgress(); setInterval(showProgress, 100); });
 
 function showProgress() {
     var result = oneLinePercentage() * 100;
     document.getElementById("title").innerHTML = new Date().getFullYear() + " Progress";
     document.getElementById("progressBar").value = result;
-    document.getElementById("p").innerHTML = result.toString().substr(0,10) + "%";
+    document.getElementById("p").innerHTML = result.toString().substr(0,11) + "%";
     console.log(result);
 }
 
